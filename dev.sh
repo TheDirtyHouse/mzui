aa5a04f268d025dcc620c336bb54875bf6fb4636
changxu <248224524@qq.com>
2018-01-05 17:07:11 +0800

    no message

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 read -p "输入1：调试组件，输入2：调试index.html， 请输入：" index
 if [ $index = "1" ]
 then
-	webpack-dev-server --env dev
+	webpack-dev-server --env dev #--host 192.168.33.31
 else
 	webpack-dev-server --env devIndex
 fi
